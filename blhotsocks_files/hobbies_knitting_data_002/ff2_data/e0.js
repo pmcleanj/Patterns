a9fe8679c7874f25c11d57922bcd8c549917b90c
pmcleanj <pmcleanj@users.noreply.github.com>
2019-03-26 22:03:01 -0600

    Add files via upload

---
+++ blhotsocks_files/hobbies_knitting_data_002/ff2_data/e0.js
@@ -0,0 +1,22 @@
+
+var zzpixie = new Image();
+
+
+
+var zzStr = "s=36;z=" + Math.random();
+
+var zzDate = new Date(); 
+
+
+/*
+*/
+
+
+
+document.write("<iframe id='ad' src=\"http://view.atdmt.com/FSF/iview/ndrtnofo0240000043fsf/direct/01?click=http://xads.zedo.com//ads2/c?a=76329%3Bx=2061%3Bg=0,0%3Bc=162000038,162000038%3Bi=0%3Bn=162%3Bs=36%3Bg=254%3Bm=0%3Bw=0%3B" + escape(zzStr) + "%3Bk%3D\" width=\"120\" height=\"600\" frameborder='0' border='0' marginwidth='0' marginheight='0' scrolling='no' align='top'></iframe>");
+
+
+
+
+
+
